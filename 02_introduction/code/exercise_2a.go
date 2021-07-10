395562c0cc4b4733062d2854271d0ea3fe0ec361
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-10 20:01:13 +0200

    Exercise with string formatting
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main () {
+  // Check `go doc fmt` for string verbs
+  fmt.Println("Hello World")
+
+  fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", "André", "Burow", 18, false)
+}
