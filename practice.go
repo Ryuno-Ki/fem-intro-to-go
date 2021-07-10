c818038dbb07217b9e7dfb7b9ea446a9eba78fc8
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-10 16:08:47 +0200

    Learn more print statements
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- practice.go
+++ practice.go
@@ -9,5 +9,13 @@ import "fmt"
 // Entry point of code. Required in every go program
 // This one here battles with main.go (same directory). Comment the other one
 func main() {
+  // These print statements return number of bytes + write errors
   fmt.Println("Hello World")
+  // No newline at the end, unless manually inserted
+  fmt.Print("Gibbe")
+  fmt.Print("rish\n")
+  // Formatted printing. %s is a string verb for strings, %d for integers
+  fmt.Printf("Hello, my name is %s. Today is %d July", "André", 10)
+  // fmt.Fprint et al. write to file. Returns number of bytes + write errors
+  // fmt.Sprint et all store in character buffer and return the string
 }
