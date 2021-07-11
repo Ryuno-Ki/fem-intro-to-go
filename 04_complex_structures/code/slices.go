4cc9d01df7ec3d835d709ad969cf4199a082f3de
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-11 22:22:16 +0200

    Learn more about handling slices in Golang
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- 04_complex_structures/code/slices.go
+++ 04_complex_structures/code/slices.go
@@ -1,71 +1,78 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
+func main() {
 
-// 	var myArray [5]int
-// 	var mySlice []int
+	var myArray [5]int
+	// This causes an index out of range runtime error, since there is no
+	// indication of how large mySlice must be
+	// var mySlice []int
 
-// 	myArray[0] = 1
-// 	mySlice[0] = 1
+	myArray[0] = 1
+	// mySlice[0] = 1
 
-// 	fmt.Println(myArray)
-// 	fmt.Println(mySlice)
+	fmt.Println(myArray)
+	// fmt.Println(mySlice)
 
-// 	// ***************************
+	// ***************************
 
-// 	// var myArray [5]int
-// 	// var mySlice []int = make([]int, 5)
+	var myArray1 [5]int
+	// Make takes up to three arguments: the type, the initial length
+	// and a maximum capacity this slice can hold
+	var mySlice1 []int = make([]int, 5)
 
-// 	// fmt.Println(myArray)
-// 	// fmt.Println(mySlice)
+	fmt.Println(myArray1)
+	fmt.Println(mySlice1)
 
-// 	// ***************************
+	// ***************************
 
-// 	// var myArray [5]int
-// 	// // var mySlice []int = make([]int, 5)
-// 	// var mySlice []int = make([]int, 5, 10)
-// 	// // var mySlice = make([]int, 5, 10)
+	var myArray2 [5]int
+	var mySlice2 []int = make([]int, 5, 10)
+	// var mySlice2 = make([]int, 5, 10)
 
-// 	// myArray[0] = 1
-// 	// mySlice[0] = 1
+	myArray2[0] = 1
+	mySlice2[0] = 1
 
-// 	// fmt.Println(myArray)
-// 	// fmt.Println(mySlice)
-// 	// fmt.Println(len(mySlice))
-// 	// fmt.Println(cap(mySlice))
+	fmt.Println(myArray2)
+	fmt.Println(mySlice2)
+	// Current length
+	fmt.Println(len(mySlice2))
+	// Maximum capacity
+	fmt.Println(cap(mySlice2))
 
-// 	// ***************************
+	// ***************************
 
-// 	// fruitArray := [5]string{"banana", "pear", "apple", "kumquat", "peach"}
+	fruitArray := [5]string{"banana", "pear", "apple", "kumquat", "peach"}
 
-// 	// var splicedFruit []string = fruitArray[1:3] // ==> ["pear", "apple",]
+	var splicedFruit []string = fruitArray[1:3] // ==> ["pear", "apple",]
 
-// 	// fmt.Println(len(splicedFruit))
-// 	// fmt.Println(cap(splicedFruit))
+	fmt.Println(splicedFruit)
+	fmt.Println(len(splicedFruit))  // => 2
+	fmt.Println(cap(splicedFruit))  // => 4
 
-// 	// ***************************
+	// ***************************
 
-// 	// SEE SLIDE
+	// SEE SLIDE
 
-// 	// ***************************
+	// ***************************
 
-// 	// slice1 := []int{1, 2, 3}
-// 	// slice2 := append(slice1, 4, 5)
+	slice1 := []int{1, 2, 3}
+	// Now has the elements of slice1, plus additional elements
+	slice2 := append(slice1, 4, 5)
 
-// 	// fmt.Println(slice1, slice2)
-// 	// fmt.Println(len(slice1), cap(slice1))
-// 	// fmt.Println(len(slice2), cap(slice2))
+	fmt.Println(slice1, slice2)
+	fmt.Println(len(slice1), cap(slice1))
+	fmt.Println(len(slice2), cap(slice2))
 
-// 	// ***************************
+	// ***************************
 
-// 	// originalSlice := []int{1, 2, 3}
-// 	// destination := make([]int, len(originalSlice))
+	// originalSlice := []int{1, 2, 3}
+	// destination := make([]int, len(originalSlice))
 
-// 	// fmt.Println("Before Copy:", originalSlice, destination)
+	// fmt.Println("Before Copy:", originalSlice, destination)
 
-// 	// mysteryValue := copy(destination, originalSlice)
+	// mysteryValue := copy(destination, originalSlice)
 
-// 	// // fmt.Println("After Copy:", originalSlice, destination, mysteryValue)
-// }
+	// // fmt.Println("After Copy:", originalSlice, destination, mysteryValue)
+}
