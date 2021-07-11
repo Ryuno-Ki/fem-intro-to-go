42fb7520e954fa5db7bbb287571b69cc819f6a67
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-11 22:31:16 +0200

    Take notes on learnings regarding maps
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- 04_complex_structures/code/maps.go
+++ 04_complex_structures/code/maps.go
@@ -2,107 +2,116 @@
 
 package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
+func main() {
 
-// 	var userEmails map[int]string
+	// Key-value mapping from int to string
+	// This example panics, because the memory allocation is missing
+	// var userEmails map[int]string
 
-// 	userEmails[1] = "user1@gmail.com"
-// 	userEmails[2] = "user2@gmail.com"
+	// userEmails[1] = "user1@gmail.com"
+	// userEmails[2] = "user2@gmail.com"
 
-// 	fmt.Println(userEmails)
+	// fmt.Println(userEmails)
 
-// 	// ****************************
+	// ****************************
 
-// 	var userEmails map[int]string = make(map[int]string)
-// 	// userEmails := make(map[int]string)
+	var userEmails1 map[int]string = make(map[int]string)
+	// userEmails := make(map[int]string)
 
-// 	userEmails[1] = "user1@gmail.com"
-// 	userEmails[2] = "user2@gmail.com"
+	userEmails1[1] = "user1@gmail.com"
+	userEmails1[2] = "user2@gmail.com"
 
-// 	fmt.Println(userEmails)
+	fmt.Println(userEmails1)
 
-// 	// ****************************
+	// ****************************
 
-// 	userEmails := map[int]string{
-// 		1: "user1@gmail.com",
-// 		2: "user2@gmail.com",
-// 	}
+	// Shorthand syntax
+	userEmails2 := map[int]string{
+		1: "user1@gmail.com",
+		2: "user2@gmail.com",
+	}
 
-// 	fmt.Println(userEmails)
+	fmt.Println(userEmails2)
 
-// 	// ****************************
+	// ****************************
 
-// 	userEmails := map[int]string{
-// 		1: "user1@gmail.com",
-// 		2: "user2@gmail.com",
-// 	}
+	userEmails3 := map[int]string{
+		1: "user1@gmail.com",
+		2: "user2@gmail.com",
+	}
 
-// 	fmt.Println(userEmails)
+	fmt.Println(userEmails3)
 
-// 	fmt.Println(userEmails[1])
+	fmt.Println(userEmails3[1])
 
-// 	// ****************************
+	// ****************************
 
-// 	userEmails := map[int]string{
-// 		1: "user1@gmail.com",
-// 		2: "user2@gmail.com",
-// 	}
+	userEmails4 := map[int]string{
+		1: "user1@gmail.com",
+		2: "user2@gmail.com",
+	}
 
-// 	fmt.Println(userEmails)
+	fmt.Println(userEmails4)
 
-// 	userEmails[1] = "newUser1@gmail.com"
+	// Mutate map
+	userEmails4[1] = "newUser1@gmail.com"
 
-// 	fmt.Println(userEmails)
+	fmt.Println(userEmails4)
 
-// 	fmt.Println(userEmails[3])
+	// Key does not exist!
+	fmt.Println(userEmails4[3])
 
-// 	// ****************************
+	// ****************************
 
-// 	userEmails := map[int]string{
-// 		1: "user1@gmail.com",
-// 		2: "user2@gmail.com",
-// 	}
+	userEmails5 := map[int]string{
+		1: "user1@gmail.com",
+		2: "user2@gmail.com",
+	}
 
-// 	email1, ok := userEmails[1]
-// 	fmt.Println("Email:", email1, "Present?", ok)
+	// email1 is user1@gmail.com, ok is true, because key does exist
+	email1, ok := userEmails5[1]
+	fmt.Println("Email:", email1, "Present?", ok)
 
-// 	// email3, ok := userEmails[3]
-// 	// fmt.Println("Email", email3, "Present?", ok)
+	// otherwise it would be empty string and false
+	email3, ok := userEmails5[3]
+	fmt.Println("Email", email3, "Present?", ok)
 
-// 	// ****************************
+	// ****************************
 
-// 	userEmails := map[int]string{
-// 		1: "user1@gmail.com",
-// 		2: "user2@gmail.com",
-// 	}
+	userEmails6 := map[int]string{
+		1: "user1@gmail.com",
+		2: "user2@gmail.com",
+	}
 
-// 	if email, ok := userEmails[1]; ok {
-// 		fmt.Println(email)
-// 	} else {
-// 		fmt.Println("I don't know what you want from me")
-// 	}
+	// First assign to value of lookup, then evaluate the boolean
+	if email, ok := userEmails6[1]; ok {
+		fmt.Println(email)
+	} else {
+		fmt.Println("I don't know what you want from me")
+	}
 
-// 	// ****************************
+	// ****************************
 
-// 	userEmails := map[int]string{
-// 		1: "user1@gmail.com",
-// 		2: "user2@gmail.com",
-// 	}
+	userEmails7 := map[int]string{
+		1: "user1@gmail.com",
+		2: "user2@gmail.com",
+	}
 
-// 	delete(userEmails, 2)
+	// First the map, then the key
+	delete(userEmails7, 2)
 
-// 	fmt.Println(userEmails)
-// 	// ****************************
+	fmt.Println(userEmails7)
+	// ****************************
 
-// 	userEmails := map[int]string{
-// 		1: "user1@gmail.com",
-// 		2: "user2@gmail.com",
-// 	}
+	userEmails8 := map[int]string{
+		1: "user1@gmail.com",
+		2: "user2@gmail.com",
+	}
 
-// 	for k, v := range userEmails {
-// 		fmt.Printf("%s has an ID of %d.\n", v, k)
-// 	}
-// 	// ****************************
-// }
+	for k, v := range userEmails8 {
+		fmt.Printf("%s has an ID of %d.\n", v, k)
+	}
+	// ****************************
+}
