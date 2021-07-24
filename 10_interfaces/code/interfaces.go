16ce6a5ca3d99cc84f080850c1d5d08661664379
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-24 22:34:12 +0200

    refactor code and also document lessons about interfaces
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- 10_interfaces/code/interfaces.go
+++ 10_interfaces/code/interfaces.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 // Add a Describer interface
+// By convention, an interface is called by the job it does, normally ending on
+// 'er'.
+// Instances automatically receive the type if they implement the interface
+// (c.f. Duck Typing)
+// Interfaces allow for code reduction, by focusing on a common set of method
+// signatures
+// Therefore, if a struct implements the describe method, its instances will
+// be of type Describer. A function can then receive any instance of this
+// interface and call the describe method on it
+// Instead of having to check for the type
+type Describer interface {
+  describe() string
+}
+
+// There is an empty interface which can be thought of as TypeScript's any:
+//interface{}
+
+// You can use it like this:
+//var something = map[string]interface{}
 
 // User is a single user type
 type User struct {
@@ -31,13 +50,27 @@ func (g *Group) describe() string {
 }
 
 // Create a function that doesn't care what type you pass in as long as the type "satisfies the interface"
+// IMPORTANT: No pointer types here!
+
+// Call the describe method on the argument
+func Describe (d Describer) string {
+  return d.describe()
+}
 
 func main() {
 	u1 := User{ID: 1, FirstName: "Marilyn", LastName: "Monroe", Email: "marilyn.monroe@gmail.com"}
 	u2 := User{ID: 1, FirstName: "Humphrey", LastName: "Bogart", Email: "humphrey.bogart@gmail.com"}
 	g := Group{role: "admin", users: []User{u1, u2}, newestUser: u2, spaceAvailable: true}
+
 	describeUser := u1.describe()
 	describeGroup := g.describe()
+
+	// Pass in reference, because declared on method signature
+	describeUserUsingInterface := Describe(&u1)
+	describeGroupUsingInterface := Describe(&g)
+
 	fmt.Println(describeUser)
 	fmt.Println(describeGroup)
+	fmt.Println(describeUserUsingInterface)
+	fmt.Println(describeGroupUsingInterface)
 }
