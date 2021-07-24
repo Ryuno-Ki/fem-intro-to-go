b4a9d3f934f43f9d57de9aaf232bd2cb54d4a24a
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-24 21:09:06 +0200

    take notes on methods and refactor code
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- 09_methods/code/methods.go
+++ 09_methods/code/methods.go
@@ -8,14 +8,32 @@ type User struct {
 	FirstName, LastName, Email string
 }
 
-func describeUser(u User) string {
+type Group struct {
+  users          []User
+  spaceAvailable bool
+}
+
+// Methods are mainly concerned with state
+// So especially use them when updating internal state of an instance of a type
+// You can have methods with the same name, but not functions
+func (u *User) describe () string {
 	desc := fmt.Sprintf("Name: %s %s, Email: %s", u.FirstName, u.LastName, u.Email)
 	return desc
 }
 
+ func (g *Group) describe () string {
+  g.spaceAvailable = len(g.users) <= 2
+  return fmt.Sprintf("%t", g.spaceAvailable)
+}
+
 func main() {
 	user := User{ID: 1, FirstName: "Marilyn", LastName: "Monroe", Email: "marilyn.monroe@gmail.com"}
+	users := []User{user, user, user}
+	g := Group{ users: users, spaceAvailable: true }
 
-	desc := describeUser(user)
+	// Call mathod
+	desc := user.describe()
 	fmt.Println(desc)
+
+	fmt.Println(g.describe())
 }
