7e10472d647cb884987a62c8ecad42cac699a811
André Jaenisch <andre.jaenisch@posteo.de>
2021-07-24 23:17:59 +0200

    build a minimal HTTP Server in Go!
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
 
-func main() {
-	fmt.Println("Building a web server")
+// Go built-in types as required by function handlers
+func home (w http.ResponseWriter, r *http.Request) {
+  fmt.Println("Home!")
+}
+
+func main () {
+	// First argument is path, second argument is callback
+	http.HandleFunc("/", home)
+	fmt.Println("Server is running on port :8080")
+	// Second argument is kind of optional
+	// The ListenAndServe is always executed.
+	// If something goes wrong, it will return an error which then gets
+	// handled by log.Fatal()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
